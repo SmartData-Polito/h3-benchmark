92bacb3d81d9c52725a0ce2dd37301f68ad235ee
Gianluca Perna <49552003+GianlucaPoliTo@users.noreply.github.com>
2022-02-03 18:33:28 +0100

    Update run_every.sh

--- dash/browsertime/run_every.sh
+++ dash/browsertime/run_every.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./browsertime_run_h1_h2_h3.sh 1:2:5 bw 1 10 #http1
-./browsertime_run_h1_h2_h3.sh 1:2:5 bw 2 10 #http2
-./browsertime_run_h1_h2_h3.sh 1:2:5 bw 3 10 #Experiment http3 bandwidth 1-2-5Mbit
+./browsertime_run_h1_h2_h3.sh 1:2:5 bw 1 10 eth0 #http1
+./browsertime_run_h1_h2_h3.sh 1:2:5 bw 2 10 eth0 #http2
+./browsertime_run_h1_h2_h3.sh 1:2:5 bw 3 10 eth0 #Experiment http3 bandwidth 1-2-5Mbit
