ffee132e2f3adbe3138f481b63d36ff843eb095d
Gianluca Perna <49552003+GianlucaPoliTo@users.noreply.github.com>
2022-02-03 18:31:43 +0100

    Update run_every.sh

--- youtube/NoDocker/run_every.sh
+++ youtube/NoDocker/run_every.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./browsertime_run_h1_h2_h3_fisico.sh 5 bw 3 10
-#./browsertime_run_h1_h2_h3_fisico.sh 50:100:200 RTT 1 10
-#./browsertime_run_h1_h2_h3_fisico.sh 1:2:5 pl 1 10
+./browsertime_run_h1_h2_h3_fisico.sh 5 bw 3 10 eth0
+#./browsertime_run_h1_h2_h3_fisico.sh 50:100:200 RTT 1 10 eth0
+#./browsertime_run_h1_h2_h3_fisico.sh 1:2:5 pl 1 10 eth0
